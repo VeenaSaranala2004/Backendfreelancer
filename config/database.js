27bc065f7c98e62073921f6829670a9f701ff8ae
Veena Saranala <saranalaveena@gmail.com>
2025-04-07 13:38:01 +0530

    Updated url

--- config/database.js
+++ config/database.js
@@ -1,17 +1,19 @@
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
 
-require("dotenv").config(); 
-const { Sequelize } = require("sequelize"); 
-const sequelize = new Sequelize( 
-process.env.DB_NAME, 
-process.env.DB_USER, 
-process.env.DB_PASSWORD, 
-{ 
-host: process.env.DB_HOST, 
-dialect: process.env.DB_DIALECT, 
-} 
-); 
-sequelize 
-.authenticate() 
-.then(() => console.log("✅ Database connected")) 
-.catch((err) => console.error("❌ Database connection failed:", err)); 
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "mysql",
+    dialectOptions: {
+        ssl: {
+            require: true, // Enable SSL if Railway requires it
+            rejectUnauthorized: false,
+        },
+    },
+});
+
+sequelize
+    .authenticate()
+    .then(() => console.log("✅ Database connected"))
+    .catch((err) => console.error("❌ Database connection failed:", err));
+
 module.exports = sequelize;
